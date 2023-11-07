8b3e37fe4459a74848ec8b341f27ebba64d09b50
Okechukwu Onah <okeyski@gmail.com>
2023-11-07 20:10:43 +0100

    dictionary of possible options

---
+++ config.py
@@ -0,0 +1,18 @@
+level_three = {
+    "1": "Rusty lever: Unlocks all latches and activates a ladder.",
+    "2": "Wall switch: Releases an vertical energy beam that immobilizes anything it comes in contact with.",
+    "3": "Small can: contains a lubricating liquid.",
+    "4": "Mirror: Good for redirecting beams of light. It's latched to the wall",
+    "5": "Floor switch: Located at the centre of the hall. Opens the grate on the roof but only for a short period of time.",
+}
+level_two = {
+    "1": "Decoy: makes you invisible and creates an exact replica of you (a la Loki). A good distraction but the effects last a few seconds.",
+    "2": "A Black Mask: Makes you look like any creature you can imagine. Perfect for disguise",
+    "3": "Sticky Goo: Ensures things are held firmly in place",
+    "4": "A Bone?: ...Or what appears to look like a bone but it’s clearly enchanted",
+}
+level_one = {
+    "1": "Fuel: A small amount of fuel. Perfect for setting up a fire.",
+    "2": "A Matchstick: Serves as a fire source",
+    "3": "A Small Axe: Capable of cutting through wood and metal but has enough strength for one swing.",
+}
